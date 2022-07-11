2354ebe8ca2d6a943163902e3418de94d4f20c44
hafeez <hahmad5072@gmail.com>
2022-07-11 15:09:35 +0500

    Form validation project completed -  commit # 2

--- project/index.js
+++ project/index.js
@@ -32,7 +32,7 @@ function showSuccessOfValidField(inputField, message) {
 
 // 1. Function for "username field validation"
 
-let isUsernameValid = function usernameValidation() {
+let isUsernameValid = function () {
   if (
     usernameInputField.value.length < 6 ||
     usernameInputField.value.length > 12
@@ -49,7 +49,7 @@ let isUsernameValid = function usernameValidation() {
 
 // 2. Function for "email field validation"
 
-let isEmailValid = function emailValidation() {
+let isEmailValid = function () {
   if (
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
       emailInputField.value
@@ -64,7 +64,7 @@ let isEmailValid = function emailValidation() {
 
 // 3. Function for "password field validation"
 
-let isPasswordValid = function passwordValidation() {
+let isPasswordValid = function () {
   if (
     passwordInputField.value.length < 8 ||
     passwordInputField.value.length > 18
@@ -81,7 +81,7 @@ let isPasswordValid = function passwordValidation() {
 
 // 4. Function for "confirm password field validation"
 
-let isConfirmPasswordValid = function confirmPasswordValidation() {
+let isConfirmPasswordValid = function () {
   if (confirmPasswordInputField.value == passwordInputField.value) {
     showSuccessOfValidField(confirmPasswordInputField, "");
     return true;
@@ -93,68 +93,35 @@ let isConfirmPasswordValid = function confirmPasswordValidation() {
   }
 };
 
-// Form Validation
+// Using event delegation to add a single event listener and listener function for all input fields of the form based on the target inside the form.
+
+// Here we are actually performing input fields validation on event type "keyup" so when user is typing inside a input field, input fields validation will occur.
 
 form.addEventListener("keyup", function (event) {
   // 1. username validation
   if (event.target == usernameInputField) {
     isUsernameValid();
-    // if (
-    //   usernameInputField.value.length < 6 ||
-    //   usernameInputField.value.length > 12
-    // ) {
-    //   showErrorOfInvalidField(
-    //     usernameInputField,
-    //     "username should be 6 to 12 characters long"
-    //   );
-    // } else {
-    //   showSuccessOfValidField(usernameInputField, "");
-    // }
   }
 
   // 2. email validation
   if (event.target == emailInputField) {
     isEmailValid();
-    // if (
-    //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    //     emailInputField.value
-    //   )
-    // ) {
-    //   showSuccessOfValidField(emailInputField, "");
-    // } else {
-    //   showErrorOfInvalidField(emailInputField, "please enter valid email");
-    // }
   }
 
   // 3. password validation
   if (event.target == passwordInputField) {
     isPasswordValid();
-    // if (
-    //   passwordInputField.value.length < 8 ||
-    //   passwordInputField.value.length > 18
-    // ) {
-    //   showErrorOfInvalidField(
-    //     passwordInputField,
-    //     "password should be 8 to 18 characters long"
-    //   );
-    // } else {
-    //   showSuccessOfValidField(passwordInputField, "");
-    // }
   }
 
   // 4. confirm password validation
   if (event.target == confirmPasswordInputField) {
     isConfirmPasswordValid();
-    // if (confirmPasswordInputField.value == passwordInputField.value) {
-    //   showSuccessOfValidField(confirmPasswordInputField, "");
-    // } else {
-    //   showErrorOfInvalidField(
-    //     confirmPasswordInputField,
-    //     "password does not match"
-    //   );
-    // }
   }
 });
+
+// When user submits the form by clicking submit button, here we will check if any of the required fields is empty, if any field found empty so user will be shown the message.
+
+// If all input fields are filled by the user then will we go to next step where we will call all input fields validation functions again and check that whether all input fields validation functions are returning true, if true we will proceed further and show message to the user that the form is successfully submitted and then clear all the input fields by making its value an empty string => "".
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -176,31 +143,18 @@ form.addEventListener("submit", function (event) {
     if (confirmPasswordInputField.value.length == 0) {
       showErrorOfEmptyField(confirmPasswordInputField);
     }
-  }
-  //   if (usernameInputField.value.length == 0) {
-  //     showErrorOfEmptyField(usernameInputField);
-  //   }
-  //   if (emailInputField.value.length == 0) {
-  //     showErrorOfEmptyField(emailInputField);
-  //   }
-  //   if (passwordInputField.value.length == 0) {
-  //     showErrorOfEmptyField(passwordInputField);
-  //   }
-  //   if (confirmPasswordInputField.value.length == 0) {
-  //     showErrorOfEmptyField(confirmPasswordInputField);
-  //   }
-  else {
-    if (
-      isUsernameValid() &&
-      isEmailValid() &&
-      isPasswordValid() &&
-      isConfirmPasswordValid()
-    ) {
-      console.log(2);
-
-      document.querySelector(".form-submitted").innerText =
-        "Form Submitted successfully";
-      console.log("form submitted succesfully");
-    }
+  } else if (
+    isUsernameValid() &&
+    isEmailValid() &&
+    isPasswordValid() &&
+    isConfirmPasswordValid()
+  ) {
+    document.querySelector(".form-submitted").innerText =
+      "Form Submitted successfully";
+    console.log("form submitted succesfully");
+    usernameInputField.value = "";
+    emailInputField.value = "";
+    passwordInputField.value = "";
+    confirmPasswordInputField.value = "";
   }
 });
